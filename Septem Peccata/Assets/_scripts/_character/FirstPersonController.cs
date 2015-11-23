91ba36ada7dbe5aad326eec2a23b376bc9c5f8c2
ana-lopes <a8615@alunos.ipca.pt>
2015-11-23 10:13:06 +0000

    weapon switch fix

--- Septem Peccata/Assets/_scripts/_character/FirstPersonController.cs
+++ Septem Peccata/Assets/_scripts/_character/FirstPersonController.cs
@@ -9,23 +9,41 @@ using Random = UnityEngine.Random;
 
 public class FirstPersonController : MonoBehaviour
 {
-    [SerializeField] private bool m_IsWalking;
-    [SerializeField] private float m_WalkSpeed;
-    [SerializeField] private float m_RunSpeed;
-    [SerializeField] [Range(0f, 1f)] private float m_RunstepLenghten;
-    [SerializeField] private float m_JumpSpeed;
-    [SerializeField] private float m_StickToGroundForce;
-    [SerializeField] private float m_GravityMultiplier;
-    [SerializeField] private MouseLook m_MouseLook;
-    [SerializeField] private bool m_UseFovKick;
-    [SerializeField] private FOVKick m_FovKick = new FOVKick();
-    [SerializeField] private bool m_UseHeadBob;
-    [SerializeField] private CurveControlledBob m_HeadBob = new CurveControlledBob();
-    [SerializeField] private LerpControlledBob m_JumpBob = new LerpControlledBob();
-    [SerializeField] private float m_StepInterval;
-    [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
-    [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
-    [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
+    [SerializeField]
+    private bool m_IsWalking;
+    [SerializeField]
+    private float m_WalkSpeed;
+    [SerializeField]
+    private float m_RunSpeed;
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float m_RunstepLenghten;
+    [SerializeField]
+    private float m_JumpSpeed;
+    [SerializeField]
+    private float m_StickToGroundForce;
+    [SerializeField]
+    private float m_GravityMultiplier;
+    [SerializeField]
+    private MouseLook m_MouseLook;
+    [SerializeField]
+    private bool m_UseFovKick;
+    [SerializeField]
+    private FOVKick m_FovKick = new FOVKick();
+    [SerializeField]
+    private bool m_UseHeadBob;
+    [SerializeField]
+    private CurveControlledBob m_HeadBob = new CurveControlledBob();
+    [SerializeField]
+    private LerpControlledBob m_JumpBob = new LerpControlledBob();
+    [SerializeField]
+    private float m_StepInterval;
+    [SerializeField]
+    private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
+    [SerializeField]
+    private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
+    [SerializeField]
+    private AudioClip m_LandSound;           // the sound played when character touches back on ground.
 
     public Main main;
 
@@ -80,13 +98,15 @@ public class FirstPersonController : MonoBehaviour
         m_FovKick.Setup(m_Camera);
         m_HeadBob.Setup(m_Camera, m_StepInterval);
         m_StepCycle = 0f;
-        m_NextStep = m_StepCycle/2f;
+        m_NextStep = m_StepCycle / 2f;
         m_Jumping = false;
         m_AudioSource = GetComponent<AudioSource>();
-		m_MouseLook.Init(transform , m_Camera.transform);
-            
+        m_MouseLook.Init(transform, m_Camera.transform);
+
         rightHandActive = false;
         switchWeapon = false;
+
+        weaponState = weapon.none;
 
         if (!main.selfQuest.Done)
         {
@@ -106,7 +126,7 @@ public class FirstPersonController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if(!main.chatting)
+        if (!main.chatting)
             RotateView();
 
         // the jump state needs to read here to make sure it is not missed
@@ -148,20 +168,20 @@ public class FirstPersonController : MonoBehaviour
             GetInput(out speed);
 
         // always move along the camera forward as it is the direction that it being aimed at
-        Vector3 desiredMove = transform.forward*m_Input.y + transform.right*m_Input.x;
+        Vector3 desiredMove = transform.forward * m_Input.y + transform.right * m_Input.x;
 
         // get a normal for the surface that is being touched to move along it
         RaycastHit hitInfo;
         Physics.SphereCast(transform.position, m_CharacterController.radius, Vector3.down, out hitInfo,
-                               m_CharacterController.height/2f);
+                               m_CharacterController.height / 2f);
 
         desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
 
-        m_MoveDir.x = desiredMove.x*speed;
-        m_MoveDir.z = desiredMove.z*speed;
+        m_MoveDir.x = desiredMove.x * speed;
+        m_MoveDir.z = desiredMove.z * speed;
 
 
-       if (m_CharacterController.isGrounded)
+        if (m_CharacterController.isGrounded)
         {
             m_MoveDir.y = -m_StickToGroundForce;
 
@@ -175,41 +195,41 @@ public class FirstPersonController : MonoBehaviour
         }
         else
         {
-            m_MoveDir += Physics.gravity*m_GravityMultiplier*Time.fixedDeltaTime;
+            m_MoveDir += Physics.gravity * m_GravityMultiplier * Time.fixedDeltaTime;
         }
-        m_CollisionFlags = m_CharacterController.Move(m_MoveDir*Time.fixedDeltaTime);
+        m_CollisionFlags = m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
 
         ProgressStepCycle(speed);
         UpdateCameraPosition(speed);
 
-        WeaponSwitch();   
+        WeaponSwitch();
     }
 
 
-        private void PlayJumpSound()
+    private void PlayJumpSound()
+    {
+        m_AudioSource.clip = m_JumpSound;
+        m_AudioSource.Play();
+    }
+
+
+    private void ProgressStepCycle(float speed)
+    {
+        if (m_CharacterController.velocity.sqrMagnitude > 0 && (m_Input.x != 0 || m_Input.y != 0))
         {
-            m_AudioSource.clip = m_JumpSound;
-            m_AudioSource.Play();
+            m_StepCycle += (m_CharacterController.velocity.magnitude + (speed * (m_IsWalking ? 1f : m_RunstepLenghten))) *
+                         Time.fixedDeltaTime;
         }
 
-
-        private void ProgressStepCycle(float speed)
+        if (!(m_StepCycle > m_NextStep))
         {
-            if (m_CharacterController.velocity.sqrMagnitude > 0 && (m_Input.x != 0 || m_Input.y != 0))
-            {
-                m_StepCycle += (m_CharacterController.velocity.magnitude + (speed*(m_IsWalking ? 1f : m_RunstepLenghten)))*
-                             Time.fixedDeltaTime;
-            }
-
-            if (!(m_StepCycle > m_NextStep))
-            {
-                return;
-            }
-
-            m_NextStep = m_StepCycle + m_StepInterval;
-
-            PlayFootStepAudio();
+            return;
         }
+
+        m_NextStep = m_StepCycle + m_StepInterval;
+
+        PlayFootStepAudio();
+    }
 
 
     private void PlayFootStepAudio()
@@ -236,21 +256,21 @@ public class FirstPersonController : MonoBehaviour
         {
             return;
         }
-            if (m_CharacterController.velocity.magnitude > 0 && m_CharacterController.isGrounded)
-            {
-                m_Camera.transform.localPosition =
-                    m_HeadBob.DoHeadBob(m_CharacterController.velocity.magnitude +
-                                      (speed*(m_IsWalking ? 1f : m_RunstepLenghten)));
-                newCameraPosition = m_Camera.transform.localPosition;
-                newCameraPosition.y = m_Camera.transform.localPosition.y - m_JumpBob.Offset();
-            }
-            else
-            {
-                newCameraPosition = m_Camera.transform.localPosition;
-                newCameraPosition.y = m_OriginalCameraPosition.y - m_JumpBob.Offset();
-            }
-            m_Camera.transform.localPosition = newCameraPosition;
+        if (m_CharacterController.velocity.magnitude > 0 && m_CharacterController.isGrounded)
+        {
+            m_Camera.transform.localPosition =
+                m_HeadBob.DoHeadBob(m_CharacterController.velocity.magnitude +
+                                  (speed * (m_IsWalking ? 1f : m_RunstepLenghten)));
+            newCameraPosition = m_Camera.transform.localPosition;
+            newCameraPosition.y = m_Camera.transform.localPosition.y - m_JumpBob.Offset();
         }
+        else
+        {
+            newCameraPosition = m_Camera.transform.localPosition;
+            newCameraPosition.y = m_OriginalCameraPosition.y - m_JumpBob.Offset();
+        }
+        m_Camera.transform.localPosition = newCameraPosition;
+    }
 
 
     private void GetInput(out float speed)
@@ -258,7 +278,7 @@ public class FirstPersonController : MonoBehaviour
         // Read input
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-            
+
         bool waswalking = m_IsWalking;
 
         // On standalone builds, walk/run speed is modified by a key press.
@@ -327,15 +347,18 @@ public class FirstPersonController : MonoBehaviour
         if (Input.GetAxis("Fire1") != 0)
         {
             animator.SetBool("attack", true);
+            Attack();
             reset = false;
         }
+
         else
             animator.SetBool("attack", false);
     }
 
     private void Attack()
     {
-        if(currentWeaponState != weapon.none) {
+        if (currentWeaponState != weapon.none)
+        {
             //jogador perde sanidade quando ataca? how 2 do this
             switch (currentWeaponState)
             {
@@ -355,10 +378,10 @@ public class FirstPersonController : MonoBehaviour
 
     private void RotateView()
     {
-        if(!main.pause)
-            m_MouseLook.LookRotation (transform, m_Camera.transform);
+        if (!main.pause)
+            m_MouseLook.LookRotation(transform, m_Camera.transform);
     }
-    
+
     //só corre quando o jogador colide com alguma coisa, não o contrário
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
@@ -373,7 +396,7 @@ public class FirstPersonController : MonoBehaviour
         {
             return;
         }
-        body.AddForceAtPosition(m_CharacterController.velocity*0.1f, hit.point, ForceMode.Impulse);
+        body.AddForceAtPosition(m_CharacterController.velocity * 0.1f, hit.point, ForceMode.Impulse);
 
         //o objeto em questão tem de ter um rigidbody não kinemático because of reasons
         if (hit.gameObject.CompareTag("self quest") && main.activeQuest == Main.CurrentQuest.first)
@@ -395,57 +418,60 @@ public class FirstPersonController : MonoBehaviour
 
         if (Input.GetAxis("Mouse ScrollWheel") != 0)
         {
-            if (Input.GetAxis("Mouse ScrollWheel") > 0 && currentWeaponState != weapon.none)
+            if (Input.GetAxis("Mouse ScrollWheel") > 0 && currentWeaponState > weapon.none)
             {
                 weaponState--;
-                    
-                if(weaponState != weapon.none)
-                    animator.SetTrigger("switch");
+                animator.SetTrigger("switch");
             }
 
-            else if (Input.GetAxis("Mouse ScrollWheel") < 0 && currentWeaponState != maxWeapon)
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0 && currentWeaponState < maxWeapon)
             {
                 weaponState++;
-                    
-                if(weaponState != weapon.crucifix)
-                    animator.SetTrigger("switch");
+                animator.SetTrigger("switch");
             }
-
             //sync animações
             reset = false;
-        }      
+        }
 
         //Desliga a layer da mão direita caso o jogador esconda todas as armas
         if (weaponState != weapon.none && animator.GetLayerWeight(1) <= 1.0f)
-            {
-                if (animator.GetLayerWeight(1) < 0.5f)
-                    animator.SetLayerWeight(1, 0.5f);
-                else
-                    animator.SetLayerWeight(1, animator.GetLayerWeight(1) + 0.02f);
-            }
+        {
+            if (animator.GetLayerWeight(1) < 0.5f)
+                animator.SetLayerWeight(1, 0.5f);
+            else
+                animator.SetLayerWeight(1, animator.GetLayerWeight(1) + 0.02f);
+        }
 
-            else if (weaponState == weapon.none && animator.GetLayerWeight(1) >= 0.0f)
-            {
-                if (animator.GetLayerWeight(1) > 0.5f)
-                    animator.SetLayerWeight(1, animator.GetLayerWeight(1) - 0.02f);
-                else
-                    animator.SetLayerWeight(1, 0);
-            }
+        else if (weaponState == weapon.none && animator.GetLayerWeight(1) >= 0.0f)
+        {
+            if (animator.GetLayerWeight(1) > 0.5f)
+                animator.SetLayerWeight(1, animator.GetLayerWeight(1) - 0.02f);
+            else
+                animator.SetLayerWeight(1, 0);
+        }
+
 
         SwitchWeapon((int)weaponState - 1);
-    } 
+
+        currentWeaponState = weaponState;
+
+    }
 
     private void SwitchWeapon(int weapon)
     {
-        Debug.Log(weapon);
-        if (animator.GetCurrentAnimatorStateInfo(1).IsName("up"))
+        Debug.Log((int)weapon);
+
+        if (animator.GetCurrentAnimatorStateInfo(1).IsName("up") && weapon != -1)
         {
             foreach (GameObject weapons in weaponObjects)
                 weapons.SetActive(false);
 
             weaponObjects[weapon].SetActive(true);
+
             animator.SetInteger("weapon", weapon);
         }
+
+        else
+            animator.SetInteger("weapon", weapon);
     }
 }
-
