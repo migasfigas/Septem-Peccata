94111b4694fa7486ad10a4f38c58591e74d0ae66
ana-lopes <a8615@alunos.ipca.pt>
2015-11-25 01:20:04 +0000

    Flickering candle light
    + having fun with post-processing stuff

---
+++ Septem Peccata/Assets/_scripts/_misc/FlickeringLight.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickeringLight : MonoBehaviour
+{
+    Light light;
+    float intensity = 1f;
+    float rand;
+
+    float timer = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 0.05f)
+        {
+            rand = Random.value * 100;
+
+            light.intensity = 1.5f * Mathf.PerlinNoise(rand + 2f + Time.time, rand + 1 + Time.time * 1);
+            timer = 0;
+        }
+    }
+}
