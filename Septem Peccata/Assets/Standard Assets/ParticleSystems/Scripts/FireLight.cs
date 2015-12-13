eb6d3599b51c6a533cb0aad5a53638bc28cae352
ana-lopes <a8615@alunos.ipca.pt>
2015-12-13 19:15:24 +0000

    unity update ?
    why so many changes

--- Septem Peccata/Assets/Standard Assets/ParticleSystems/Scripts/FireLight.cs
+++ Septem Peccata/Assets/Standard Assets/ParticleSystems/Scripts/FireLight.cs
@@ -6,6 +6,8 @@ namespace UnityStandardAssets.Effects
 {
     public class FireLight : MonoBehaviour
     {
+        public float morphSpeed = .1f;
+
         private float m_Rnd;
         private bool m_Burning = true;
         private Light m_Light;
@@ -22,10 +24,10 @@ namespace UnityStandardAssets.Effects
         {
             if (m_Burning)
             {
-                m_Light.intensity = 2*Mathf.PerlinNoise(m_Rnd + Time.time, m_Rnd + 1 + Time.time*1);
-                float x = Mathf.PerlinNoise(m_Rnd + 0 + Time.time*2, m_Rnd + 1 + Time.time*2) - 0.5f;
-                float y = Mathf.PerlinNoise(m_Rnd + 2 + Time.time*2, m_Rnd + 3 + Time.time*2) - 0.5f;
-                float z = Mathf.PerlinNoise(m_Rnd + 4 + Time.time*2, m_Rnd + 5 + Time.time*2) - 0.5f;
+                m_Light.intensity = 2*Mathf.PerlinNoise(m_Rnd + Time.time*morphSpeed, m_Rnd + 1 + Time.time*morphSpeed*1);
+                float x = Mathf.PerlinNoise(m_Rnd + 0 + Time.time*morphSpeed*2, m_Rnd + 1 + Time.time*morphSpeed*2) - 0.5f;
+                float y = Mathf.PerlinNoise(m_Rnd + 2 + Time.time*morphSpeed*2, m_Rnd + 3 + Time.time*morphSpeed*2) - 0.5f;
+                float z = Mathf.PerlinNoise(m_Rnd + 4 + Time.time*morphSpeed*2, m_Rnd + 5 + Time.time*morphSpeed*2) - 0.5f;
                 transform.localPosition = Vector3.up + new Vector3(x, y, z)*1;
             }
         }
